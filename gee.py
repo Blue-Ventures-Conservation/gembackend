33818a0581908eb2e36561f512b97fc4935ef36e
zibnix <cfexit@protonmail.com>
2023-02-05 16:26:43 -0800

    use fiebase sevrice account with gee, so that access to cloud storage via GEE should work

--- gee.py
+++ gee.py
@@ -15,8 +15,8 @@ ls8_dataset = "LANDSAT/LC08/C02/T1_L2"
 ls9_dataset = "LANDSAT/LC09/C02/T1_L2"
 
 def init_gee():
-    service_account = 'backend@crflask.iam.gserviceaccount.com'
-    credentials = ee.ServiceAccountCredentials(service_account, 'gee_service_account.json')
+    service_account = 'firebase-adminsdk-7r5sg@crflask.iam.gserviceaccount.com'
+    credentials = ee.ServiceAccountCredentials(service_account, 'firebase_service_account.json')
     ee.Initialize(credentials)
 
 def known_mangroves() -> ee.Image:
