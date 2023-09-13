f4200db245d7fe529dbab419bfc0f4eeeff02650
zibnix <cfexit@protonmail.com>
2023-09-12 17:28:45 -0700

    speed up dynamics by fetching everything in one request

--- dynamics.py
+++ dynamics.py
@@ -1,5 +1,6 @@
 import ee
 import time
+import uuid
 
 from typing import List
 
@@ -36,26 +37,39 @@ def get_dynamics(uid: str, target_class: str, sub_regions: List[dict], red: str,
         
         roi_lpg = lpg_do(ctarget, htarget, lmask, pmask, gmask, coast)
         
-        output = {
-                "name": roi["name"],
-                "stats": {
-                    "contemporary_area": roi_lpg["contemporary_area"],
-                    "historical_area": roi_lpg["historical_area"],
-                    "loss": roi_lpg["loss"],
-                    "persistence": roi_lpg["persistence"],
-                    "gain": roi_lpg["gain"],
-                },
-                "loss_url": lpg_url(lmask, red),
-                "persistence_url": lpg_url(pmask, green),
-                "gain_url": lpg_url(gmask, blue),
-                "created_at": int(time.time()),
-                "timeout": tile_timeout,
-                "sub_region_stats": []
-        }
+        slug = uuid.uuid4().hex
+        everything = ee.Dictionary({
+            slug: roi_lpg,
+        })
         
         for sr in sub_regions:
             name = sr["name"]
             lpg = lpg_do(ctarget, htarget, lmask, pmask, gmask, ee.Geometry(sr["geometry"]))
+            everything = everything.set(name, lpg)
+        
+        everything = everything.getInfo()
+        roi_lpg = everything[slug]
+        
+        output = {
+            "name": roi["name"],
+            "stats": {
+                "contemporary_area": roi_lpg["contemporary_area"],
+                "historical_area": roi_lpg["historical_area"],
+                "loss": roi_lpg["loss"],
+                "persistence": roi_lpg["persistence"],
+                "gain": roi_lpg["gain"],
+            },
+            "loss_url": lpg_url(lmask, red),
+            "persistence_url": lpg_url(pmask, green),
+            "gain_url": lpg_url(gmask, blue),
+            "created_at": int(time.time()),
+            "timeout": tile_timeout,
+            "sub_region_stats": []
+        }
+        
+        for sr in sub_regions:
+            name = sr["name"]
+            lpg = everything[name]
             output["sub_region_stats"].append({"name": name, "contemporary_area": lpg["contemporary_area"], "historical_area": lpg["historical_area"], "loss": lpg["loss"], "persistence": lpg["persistence"], "gain": lpg["gain"]})
         
         return output
@@ -83,20 +97,20 @@ def lpg_url(mask: ee.Image, color: str) -> str:
     vis = {"palette": color}
     return mask.getMapId(vis)["tile_fetcher"].url_format
 
-def lpg_do(cont_mask: ee.Image, hist_mask: ee.Image, loss_mask: ee.Image, persist_mask: ee.Image, gain_mask: ee.Image, geo: ee.Geometry) -> dict:
+def lpg_do(cont_mask: ee.Image, hist_mask: ee.Image, loss_mask: ee.Image, persist_mask: ee.Image, gain_mask: ee.Image, geo: ee.Geometry) -> ee.Dictionary:
     ctotal = lpg_reduce(geo, cont_mask)
     htotal = lpg_reduce(geo, hist_mask)
     loss = lpg_reduce(geo, loss_mask)
     persist = lpg_reduce(geo, persist_mask)
     gain = lpg_reduce(geo, gain_mask)
     
-    return {
+    return ee.Dictionary({
         "contemporary_area": ctotal,
         "historical_area": htotal,
         "loss": loss,
         "persistence": persist,
         "gain": gain,
-    }
+    })
 
 def lpg_reduce(geo: ee.Geometry, mask: ee.Image) -> float:
     return ee.Image.pixelArea().updateMask(mask).reduceRegion(
@@ -106,4 +120,4 @@ def lpg_reduce(geo: ee.Geometry, mask: ee.Image) -> float:
             maxPixels = 1e13,
             bestEffort = True,
             tileScale = 8,
-    ).get("area").getInfo()
+    ).get("area")
