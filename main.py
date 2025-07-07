c5c404169c6a9ac5a1330327f259c7e5d42b3ef2
zibnix <cfexit@protonmail.com>
2025-07-06 23:28:25 -0700

    args before kwargs

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ if "gunicorn" in os.environ.get("SERVER_SOFTWARE", ""): # prod
     project.project_id = firebase_app.project_id
     accessor = Accessor()
     credentials, _ = google.auth.default()
-    ee.Initialize(project=project.project_id, credentials)
+    ee.Initialize(credentials, project=project.project_id)
     is_debug = False
 
 def token_check(func):
